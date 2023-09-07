e297ea840f7e2ebfbf90e1be70708bd09c5d78c0
Lexzino <alexudeze001@gmal.com>
2023-09-07 10:06:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return(0);
+}	
+
