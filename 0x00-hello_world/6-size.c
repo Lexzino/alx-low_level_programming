8714e5723206cafb5e3436943de254ddbb9033e7
Lexzino <alexudeze001@gmal.com>
2023-09-07 11:49:31 -0700

    "6-size.c"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print the size of various types a based on 
+ * the computer it is compiled and run on..
  *
  * Return: Always 0 (Success)
  */
