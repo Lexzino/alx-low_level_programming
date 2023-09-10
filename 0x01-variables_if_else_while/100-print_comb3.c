76578006b7d8bedea82e4f79b54b9c2d82483d43
Lexzino <alexudeze001@gmal.com>
2023-09-10 02:17:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,8 @@
  */
 
 int main(void)
-	
 {
 	int d, p;
-	
 	for (d = '0'; d < '9'; d++)
 	{
 	for (p = d + 1; p <= '9'; p++)
@@ -21,7 +19,7 @@ int main(void)
 	{
 	putchar(d);
 	putchar(p);
-	if (d == '8' && p =='9')
+	if (d == '8' && p == '9')
 	continue;
 	putchar(',');
 	putchar(' ');
