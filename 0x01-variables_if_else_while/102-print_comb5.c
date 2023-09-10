51484e86e29ce3696f932c9ba4abf5b11de95b9c
Lexzino <alexudeze001@gmal.com>
2023-09-10 03:18:49 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,8 +15,7 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-	
-	for (q = p +1; q <= 99; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
@@ -25,6 +24,8 @@ int main(void)
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
 	continue;
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	putchar('\n');
