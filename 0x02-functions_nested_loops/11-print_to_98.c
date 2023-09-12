43c2e182fa5fa1660cfc2a44010d301c73cc0ee1
Lexzino <alexudeze001@gmal.com>
2023-09-12 08:41:45 -0700

    "11-print_to_98.c"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -31,7 +31,7 @@ void print_to_98(int n)
 	{
 	for (; n >= 98; n--)
 	{
-	if (n ==98)
+	if (n == 98)
 	{
 	printf("%d", n);
 	printf("\n");
@@ -43,4 +43,4 @@ void print_to_98(int n)
 	}
 	}
 	}
-}	
+}
