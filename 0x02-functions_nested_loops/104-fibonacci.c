f36efcd4a93aebc38dd4aad8dad31fa802fca887
Lexzino <alexudeze001@gmal.com>
2023-09-12 11:41:42 -0700

    "104-fibonacci.c"

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int mian(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
 	for (count = 0; count <92; count++)
