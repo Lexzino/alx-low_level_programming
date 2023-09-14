b151f2f1d20b6474eb4a55be34bfc0fb619d5739
Lexzino <alexudeze001@gmal.com>
2023-09-14 07:59:41 -0700

    "2-largest_number.c"

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
     if (a >= b && b >= c)
     {
-        largest = a;
+	largest = a;
     }
     else if (b >= a && a >= c)
     {
-        largest = b;
+	largest = b;
     }
     else
     {
