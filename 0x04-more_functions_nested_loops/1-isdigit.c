0282db34499a3aa6c0faa64209bb3302df1a8d01
Lexzino <alexudeze001@gmal.com>
2023-09-14 11:35:14 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @x: The number to be character
+ * @x: The number to be checked
  * Return: 1 for a character that will be a digit or 0 for any else
  */
 
