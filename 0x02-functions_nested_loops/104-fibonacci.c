f0edfb04ad1b8351ef6f211bf375b9825e1327a1
Lexzino <alexudeze001@gmal.com>
2023-09-17 17:07:05 -0700

    nested loops updae

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,14 @@
  */
 
 int mian(void)
+
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
-	for (count = 0; count <92; count++)
+	for (count = 0; count < 92; count++)
 	{
 	sum = fib1 + fib2;
 	printf("%lu, ", sum);
@@ -36,7 +37,7 @@ int mian(void)
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
-	printf(",");
+	printf(", ");
 	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
 	fib2_half1 = half1;
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,5 +11,7 @@
 int _putchar(char c)
 
 {
+
 	return (write(1, &c, 1));
+
 }
