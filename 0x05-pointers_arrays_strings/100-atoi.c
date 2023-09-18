9d42e73f13fe555c2f15f6503203fd7b028e7da4
Lexzino <alexudeze001@gmal.com>
2023-09-18 03:51:07 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,12 +3,13 @@
 
 
 /**
- * _ato1 - Convert a string to an innteger.
+ * _atoi - Convert a string to an innteger.
  * @s: The pointer to convert
  * Return: A integer
  */
 
 int _atoi(char *s)
+
 {
 	int c = 0;
 	unsigned int ni = 0;
