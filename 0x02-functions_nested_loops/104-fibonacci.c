d9d2aa320348689f32dcdb8ebb60d2459f0fdd23
Lexzino <alexudeze001@gmal.com>
2023-09-17 17:57:52 -0700

    nested loops update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,8 +2,7 @@
 
 
 /**
- * main - Print the first 98 Fibonacci numbers, starting with
- * 1 and 2, seperated by a comma followed by a space.
+ * main - Print the first 98 Fibonacci numbers, starting with 1 and 2
  * Return: Always 0.
  */
 
