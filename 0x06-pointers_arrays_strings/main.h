a6466f0fb275bc0fa6bc58671ed081982f282074
Lexzino <alexudeze001@gmal.com>
2023-09-20 11:04:46 -0700

    main.h and readme update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -18,4 +16,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int sie);
 
 #endif
-
