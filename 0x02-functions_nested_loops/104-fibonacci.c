61ecfcfd6118dc06a9e5888c12e601075dc3b54a
Lexzino <alexudeze001@gmal.com>
2023-09-20 06:54:15 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n1 = 0, n2 = 1, n3, count, num;
 
-	printf("Enter the number of terms to be printed\n");
+	printf("Prints the first two Fibonacci numbers");
 	scanf("%d", &num);
 
 	printf("\n%d\n%d\n", n1, n2);
