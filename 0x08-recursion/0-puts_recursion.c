60f5aec68f2a2ae9e387eee42c8d99e1712b846b
Lexzino <alexudeze001@gmal.com>
2023-09-28 08:09:14 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Alwyas 0 (Success)
+ */
+
+void _puts_recursion(char *s);
+
+{
+	if (*s)
+	{
+	_putchar(*s)
+	_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n')
+}
