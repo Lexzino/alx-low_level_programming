6e589659ba6ae0b47c8d3975f918a64c4b3591f9
Lexzino <alexudeze001@gmal.com>
2023-09-28 08:26:55 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,19 @@
 
 
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Alwyas 0 (Success)
+ * _puts_recursion - Print a string followed by a new line
+ * @s: the string to print
+ * Return: Nothing.
  */
 
-void _puts_recursion(char *s);
-
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s =='\0')
 	{
-	_putchar(*s)
-	_puts_recursion(s + 1);
+	_putchar('\n');
+	return;
 	}
-	else
-	_putchar('\n')
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
