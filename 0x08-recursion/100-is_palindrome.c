631e1474c36a56529c869e3c99fd13802ba076c0
Lexzino <alexudeze001@gmal.com>
2023-09-28 13:57:10 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -20,13 +20,13 @@ int _strlen_recursion(char *s)
  * @n2: biggest iterator.
  * Return: .
  */
-int comparator(char*s, int n1, int n2)
+int comparator(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *(s +n2))
+	if (*(s + n1) == *(s + n2))
 	{
 	if (n1 == n2 || n1 == n2 + 1)
 	return (1);
-	return(0 + comparator(s, n1 + 1, n2 - 1));
+	return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
