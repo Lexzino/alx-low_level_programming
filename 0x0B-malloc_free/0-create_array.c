80656ab836f6c784c3b5823eb0ec927666fae325
Lexzino <alexudeze001@gmal.com>
2023-10-04 00:28:12 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;	
-	unsigned int i;
+	unsigned int a;
 
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 	return (NULL);	
-	for (i = 0; i < size; i++)
-	str[i] = c;
+	for (a = 0; a < size; a++)
+	str[a] = c;
 	return (str);	
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**		
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0	
+ */
+
+char *_strdup(char *str)
+{
+	char *aaa;
+	int i, c = 0;
+
+	if (str == NULL)
+	return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	i++;
+	aaa = malloc(sizeof(char) * (i + 1));
+	if (aaa == NULL)
+	return (NULL);	
+
+	for (c = 0; str[c]; c++)
+	aaa[c] = str[c];
+	return (aaa);
+}
