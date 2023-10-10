98783e761695b8f5229f256c5e73d06b9b878c37
Lexzino <alexudeze001@gmal.com>
2023-10-10 11:44:30 -0700

    0x0E completed

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+/**
+  * new_dog - ...
+  * @name: ...
+  * @age: ...
+  * @owner: ...
+  * Return: ...
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *max_dog;
+	int name_l = 0, own_l = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+	name_l = _strlen(name) + 1;
+	own_l = _strlen(owner) + 1;
+	max_dog = malloc(sizeof(dog_t));
+	if (max_dog == NULL)
+	return (NULL);
+	max_dog->name = malloc(sizeof(char) * name_l);
+	if (max_dog->name == NULL)
+	{
+	free(max_dog);
+	return (NULL);
+	}
+	max_dog->owner = malloc(sizeof(char) * own_l);
+	if (max_dog->owner == NULL)
+	{
+	free(max_dog->name);
+	free(max_dog);
+	return (NULL);
+	}
+	max_dog->name = _strcpy(max_dog->name, name);
+	max_dog->owner = _strcpy(max_dog->owner, owner);
+	max_dog->age = age;
+	}
+	return (max_dog);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  * Return: String length
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s != '\0'; s++)
+	{
+	i++;
+	}
+	return (i);
+}
+
+/**
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  * Return: the pointer to dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+	dest[a] = src[a];
+	}
+	dest[a++] = '\0';
+	return (dest);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+#0x0E. C - Structures, typedef
+
+##Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+##General
+What are structures, when, why and how to use them
+How to use typedef
