d169198be2450a8dbba45fd2b12b0910ae7923cf
Lexzino <alexudeze001@gmal.com>
2023-10-26 10:33:36 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int c;
+
+	num = 1;
+	for (c = 1; c <= power; c++)
+	num *= base;
+	return (num);
+}
+
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+	check = n & divisor;
+	if (check == divisor)
+	{
+	flag = 1;
+	_putchar('1');
+	}
+	else if (flag == 1 || divisor == 1)
+	{
+	_putchar('0');
+	}
+	divisor >>= 1;
+	}
+}
