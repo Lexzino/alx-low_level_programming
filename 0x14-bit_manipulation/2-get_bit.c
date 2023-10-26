2798a2d2b5a1e79f8237040757b91ad64cfa7db9
Lexzino <alexudeze001@gmal.com>
2023-10-26 10:40:26 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,6 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 	return (-1);
-	
 	divisor = 1 << index;
 	check = n & divisor;
 	if (check == divisor)
