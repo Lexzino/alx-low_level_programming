943817af5a8765655d33e64831d1644b91985386
Lexzino <alexudeze001@gmal.com>
2023-10-30 16:50:38 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 
+
 /**
  * error_file - checks if files can be opened.
  * @file_from: file_from.
@@ -24,6 +25,7 @@ void error_file(int file_from, int file_to, char *argv[])
 	}
 }
 
+
 /**
  * main - check the code for Holberton School students.
  * @argc: number of arguments.
@@ -42,9 +44,11 @@ int main(int argc, char *argv[])
 	dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 	exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
+
 	nchars = 1024;
 	while (nchars == 1024)
 	{
@@ -55,12 +59,14 @@ int main(int argc, char *argv[])
 	if (nwr == -1)
 	error_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 	exit(100);
 	}
+
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
